4d52ce6366d8031593fb7e12ab3a2ea7a25497f1
Omri Horowitz <horowitzomri@gmail.com>
2021-02-09 14:25:11 -0800

    Initial Commit

---
+++ DatingOutcomeCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DatingOutcomeCalculator
+//
+//  Created by Omri Horowitz on 2/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
